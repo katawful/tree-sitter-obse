97d67b29253569724e2101956b15c5a411035ce8
katawful <katisntgood@gmail.com>
2021-04-19 11:25:21 -0400

    added case insensive function This will make some keywords a lot simpler to use

--- grammar.js
+++ grammar.js
@@ -17,8 +17,8 @@ module.exports = grammar({
     ),
 
     script_declarer: $ => choice(
-      /[sS][cC][nN]/,
-      /[sS][cC][rR][iI][pP][tT][nN][aA][mM][eE]/
+      caseInsensitive('scn'),
+      caseInsensitive('scriptname'),
     ),
 
     script_body: $ => repeat1($._top_level_items),
@@ -30,7 +30,7 @@ module.exports = grammar({
 
     var_declare: $ => seq(
       field('type', $._type),
-      field('variable', $.variable),
+      field('variable', $.identifier),
     ),
 
     identifier: $ => /[a-zA-Z_]\w*/,
@@ -43,97 +43,21 @@ module.exports = grammar({
     ),
 
     int_type: $ => choice(
-      /[iI][nN][tT]/,
-      /[lL][oO][nN][gG]/,
-      /[sS][hH][oO][rR][tT]/,
+      caseInsensitive('int'),
+      caseInsensitive('short'),
+      caseInsensitive('long'),
     ),
-    float_type: $ => /[fF][lL][oO][aA][tT]/,
-    string_type: $ => /[sS][tT][rR][iI][nN][gG]_[vV][aA][rR]/,
-    array_type: $ => /[aA][rR][rR][aA][yY]_[vV][aA][rR]/,
-
-    variable: $ => /[a-zA-Z_]\w+/,
+    float_type: $ => caseInsensitive('float'),
+    string_type: $ => caseInsensitive('string_var'),
+    array_type: $ => caseInsensitive('array_var'),
 
     block: $ => seq(
-      /[bB][eE][gG][iI][nN]/,
-      field('block_type', $.block_type),
+      caseInsensitive('begin'),
+      field('block_type', $.identifier),
       field('body', $.body_repeat),
-      /[eE][nN][dD]/,
+      caseInsensitive('end'),
     ),
 
-    block_type: $ => choice(
-      /[EE][xX][iI][tT][GG][aA][mM][eE]/,
-      /[EE][xX][iI][tT][TT][oO][MM][aA][iI][nN][MM][eE][nN][uU]/,
-      /[FF][uU][nN][cC][tT][iI][oO][nN]/,
-      /[GG][aA][mM][eE][MM][oO][dD][eE]/,
-      /[LL][oO][aA][dD][GG][aA][mM][eE]/,
-      /[MM][eE][nN][uU][MM][oO][dD][eE]/,
-      /[OO][nN][AA][cC][tT][iI][vV][aA][tT][eE]/,
-      /[OO][nN][AA][cC][tT][oO][rR][DD][rR][oO][pP]/,
-      /[OO][nN][AA][cC][tT][oO][rR][EE][qQ][uU][iI][pP]/,
-      /[OO][nN][AA][cC][tT][oO][rR][UU][nN][eE][qQ][uU][iI][pP]/,
-      /[OO][nN][AA][dD][dD]/,
-      /[OO][nN][AA][lL][aA][rR][mM]/,
-      /[OO][nN][AA][lL][aA][rR][mM][TT][rR][eE][sS][pP][aA][sS][sS]/,
-      /[OO][nN][AA][lL][aA][rR][mM][VV][iI][cC][tT][iI][mM]/,
-      /[OO][nN][AA][tT][tT][aA][cC][kK]/,
-      /[OO][nN][BB][lL][oO][cC][kK]/,
-      /[OO][nN][BB][oO][wW][AA][tT][tT][aA][cC][kK]/,
-      /[OO][nN][CC][lL][iI][cC][kK]/,
-      /[OO][nN][CC][lL][oO][sS][eE]/,
-      /[OO][nN][CC][rR][eE][aA][tT][eE][PP][oO][tT][iI][oO][nN]/,
-      /[OO][nN][CC][rR][eE][aA][tT][eE][SS][pP][eE][lL][lL]/,
-      /[OO][nN][DD][eE][aA][tT][hH]/,
-      /[OO][nN][DD][oO][dD][gG][eE]/,
-      /[OO][nN][DD][rR][iI][nN][kK][PP][oO][tT][iI][oO][nN]/,
-      /[OO][nN][DD][rR][oO][pP]/,
-      /[OO][nN][EE][aA][tT][II][nN][gG][rR][eE][dD][iI][eE][nN][tT]/,
-      /[OO][nN][EE][nN][cC][hH][aA][nN][tT]/,
-      /[OO][nN][EE][qQ][uU][iI][pP]/,
-      /[OO][nN][FF][aA][lL][lL][II][mM][pP][aA][cC][tT]/,
-      /[OO][nN][HH][eE][aA][lL][tT][hH][DD][aA][mM][aA][gG][eE]/,
-      /[OO][nN][HH][iI][tT]/,
-      /[OO][nN][HH][iI][tT][WW][iI][tT][hH]/,
-      /[OO][nN][KK][nN][oO][cC][kK][oO][uU][tT]/,
-      /[OO][nN][LL][oO][aA][dD]/,
-      /[OO][nN][MM][aA][gG][iI][cC][AA][pP][pP][lL][yY]/,
-      /[OO][nN][MM][aA][gG][iI][cC][CC][aA][sS][tT]/,
-      /[OO][nN][MM][aA][gG][iI][cC][EE][fF][fF][eE][cC][tT][HH][iI][tT]/,
-      /[OO][nN][MM][aA][gG][iI][cC][EE][fF][fF][eE][cC][tT][HH][iI][tT]2/,
-      /[OO][nN][MM][aA][pP][MM][aA][rR][kK][eE][rR][AA][dD][dD]/,
-      /[OO][nN][MM][oO][uU][sS][eE][oO][vV][eE][rR]/,
-      /[OO][nN][MM][uU][rR][dD][eE][rR]/,
-      /[OO][nN][NN][eE][wW][GG][aA][mM][eE]/,
-      /[OO][nN][OO][pP][eE][nN]/,
-      /[OO][nN][PP][aA][cC][kK][aA][gG][eE][CC][hH][aA][nN][gG][eE]/,
-      /[OO][nN][PP][aA][cC][kK][aA][gG][eE][DD][oO][nN][eE]/,
-      /[OO][nN][PP][aA][cC][kK][aA][gG][eE][SS][tT][aA][rR][tT]/,
-      /[OO][nN][QQ][uU][eE][sS][tT][CC][oO][mM][pP][lL][eE][tT][eE]/,
-      /[OO][nN][RR][eE][cC][oO][iI][lL]/,
-      /[OO][nN][RR][eE][lL][eE][aA][sS][eE]/,
-      /[OO][nN][RR][eE][sS][eE][tT]/,
-      /[OO][nN][SS][aA][vV][eE][II][nN][iI]/,
-      /[OO][nN][SS][cC][rR][iI][pP][tT][eE][dD][SS][kK][iI][lL][lL][UU][pP]/,
-      /[OO][nN][SS][cC][rR][oO][lL][lL][CC][aA][sS][tT]/,
-      /[OO][nN][SS][eE][lL][lL]/,
-      /[OO][nN][SS][kK][iI][lL][lL][UU][pP]/,
-      /[OO][nN][SS][oO][uU][lL][TT][rR][aA][pP]/,
-      /[OO][nN][SS][pP][eE][lL][lL][CC][aA][sS][tT]/,
-      /[OO][nN][SS][tT][aA][gG][gG][eE][rR]/,
-      /[OO][nN][SS][tT][aA][rR][tT][CC][oO][mM][bB][aA][tT]/,
-      /[OO][nN][TT][rR][iI][gG][gG][eE][rR]/,
-      /[OO][nN][TT][rR][iI][gG][gG][eE][rR][AA][cC][tT][oO][rR]/,
-      /[OO][nN][TT][rR][iI][gG][gG][eE][rR][MM][oO][bB]/,
-      /[OO][nN][UU][nN][eE][qQ][uU][iI][pP]/,
-      /[OO][nN][VV][aA][mM][pP][iI][rR][eE][FF][eE][eE][dD]/,
-      /[OO][nN][WW][aA][tT][eE][rR][DD][iI][vV][eE]/,
-      /[OO][nN][WW][aA][tT][eE][rR][SS][uU][rR][fF][aA][cC][eE]/,
-      /[PP][oO][sS][tT][LL][oO][aA][dD][GG][aA][mM][eE]/,
-      /[QQ][QQ][QQ]/,
-      /[SS][aA][vV][eE][GG][aA][mM][eE]/,
-      /[SS][cC][rR][iI][pP][tT][EE][fF][fF][eE][cC][tT][FF][iI][nN][iI][sS][hH]/,
-      /[SS][cC][rR][iI][pP][tT][EE][fF][fF][eE][cC][tT][SS][tT][aA][rR][tT]/,
-      /[SS][cC][rR][iI][pP][tT][EE][fF][fF][eE][cC][tT][UU][pP][dD][aA][tT][eE]/,
-    ),
     body_repeat: $ => repeat1($.body),
 
     body: $ => choice(
@@ -148,17 +72,28 @@ module.exports = grammar({
     ),
 
     set_statement: $ => seq(
-      /[sS][eE][tT]/,
-      field('variable', $.variable),
-      /[tT][oO]/,
+      caseInsensitive('set'),
+      field('variable', $.identifier),
+      caseInsensitive('to'),
     ),
     let_statement: $ => seq(
-      'let',
-      $.variable,
-      /[tT][oO]/,
+      caseInsensitive('let'),
+      $.identifier,
+      // TODO make sure to add in expressions here
+      ':='
     ),
     function_call: $ => 'tes',
     loop: $ => 'test',
     conditional: $ => 'tesa',
   }
 });
+
+function caseInsensitive (keyword, aliasAsWord = true) {
+let result = new RegExp(keyword
+  .split('')
+  .map(l => l !== l.toUpperCase() ? `[${l}${l.toUpperCase()}]` : l)
+  .join('')
+)
+if (aliasAsWord) result = alias(result, keyword)
+return result
+}
